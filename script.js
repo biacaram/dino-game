265fdb37f6fce67b4858240ccc5396719a27e7a1
Beatriz Andre <biacaram@gmail.com>
2022-03-25 23:00:52 -0300

    game over

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector(".dino");
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
@@ -11,7 +12,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
     isJumping = true;
 
     let upInterval = setInterval(() => {
@@ -46,6 +46,9 @@ function createCactus() {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">game over :(</h1>';
         } else {
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
@@ -56,4 +59,4 @@ function createCactus() {
 }
 
 createCactus();
-document.addEventListener('keyup', handleKeyUp);
+document.addEventListener('keydown', handleKeyUp);
